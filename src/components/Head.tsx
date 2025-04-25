d11e46b5779212f5ec48a3c49cfbe14f197071cb
Kieron <kieronvonb@gmail.com>
2025-04-25 12:59:42 -0400

    fixed issue with hemisphereLight in Head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -29,7 +29,7 @@ export default function Head() {
     <div className="relative z-10 w-full h-full p-[3vh] sm:p-[0] flex items-center aspect-square">
       <Canvas camera={{ position: [0, 0, 4], fov:30 }}>
         <Suspense fallback={null}>
-          <hemisphereLight skyColor={"#ffffff"} groundColor={"#ffffff"} intensity={0.8} />
+          <hemisphereLight args={["#ffffff", "#ffffff", 0.8]} />
           <HeadModelWithLight />
           {/* OrbitControls will rotate the entire group */}
           <OrbitControls enableZoom={false} enableRotate={true} />
