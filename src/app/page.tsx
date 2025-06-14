0e0c27425f1499c024c61f7506eeea83c306ea9e
Kieron <kieronvonb@gmail.com>
2025-06-14 15:24:10 -0400

    changed mouse over head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,11 +20,13 @@ export default function Home() {
         <div className="hidden xl:block flex w-full"></div>
         {/* On mobile, align top with extra padding; on md+, center with smaller padding */}
         <div className="flex w-full items-start justify-center pt-[12vh] md:items-center md:pt-[5vh]">
-	 	 	<div className="relative flex w-[40vh] md:w-[50vh] 2xl:w-[60vh] aspect-square items-center justify-center">
-	  			<Circle />
-	  			<Head />
-				<Title />
-	  		</div>
+          <div
+            className="relative flex w-[40vh] md:w-[50vh] 2xl:w-[60vh] aspect-square items-center justify-center xl:cursor-grab xl:active:cursor-grabbing"
+          >
+            <Circle />
+            <Head />
+            <Title />
+          </div>
 		</div>
 	  	<h1 className="absolute flex hidden xl:block p-standard absolute bottom-0 left-0 text-[20vw] 2xl:text-[25vh] leading-[0.7] tracking-[-0.05em] font-headings">
 	  		Kieron <br></br> von Buchstab
