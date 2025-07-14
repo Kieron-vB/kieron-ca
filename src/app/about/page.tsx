e6db187791fe08683d83ecf7fc85e1e7c7c8d5d4
Kieron <kieronvonb@gmail.com>
2025-07-14 14:33:38 -0400

    Updated About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function About() {
 			</h1>
 			<div className="flex flex-wrap 2xl:flex-nowrap gap-x-[3vw] gap-y-[5vh] md:gap-y-[8vh] items-start">
 				<p className="flex-l sm:max-w-[100vw] 2xl:max-w-[50vw] pt-standard font-body leading-paragraph tracking-paragraph text-text text-mobile lg:text-desktop">
-				Kieron von Buchstab is many things. He's a student, a developer, a hobbyist, a lobbyist, a freelance lobotomist, and a part time rocket scientist. He likes his coffee black, his tea green, and currently his favorite animal is the tree kangaroo. In his free time he enjoys writing, programming, and camping. He's currently in his third year of aerospace engineering at Carleton University but might drop out if he gets bored or something more fun comes up.
+				Kieron von Buchstab is many things. He's a student, a developer, a hobbyist, a lobbyist, a freelance lobotomist, and a part time rocket scientist. He likes his coffee black, his tea green, and currently his favorite animal is the tree kangaroo. In his free time he enjoys writing, programming, and camping. He's currently going into his final year of aerospace engineering at Carleton University and is on track to graduate in the spring of 2026.
 				</p>
 				<div className="flex-col self-center w-full" style={{ transform: 'rotate(5deg)' }}>
 					<img
