f648fe23c1c259eaa9dca5a02d3fba4280cbb513
Kieron-vB <kierononvb@gmail.com>
2026-05-23 22:28:11 -0400

    Changed font, About, Contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function About() {
 			</h1>
 			<div className="flex flex-wrap gap-[10rem]">
 			<p className="text-center w-full pt-[4rem] px-standard font-body leading-paragraph tracking-paragraph text-text text-mobile lg:text-desktop">
-				If you want to contact Kieron about business propositions, top secret missions, his stance on magicians, or a personal admission you can email him at <span className="text-link"> kieron@kieron.ca</span>.			</p>
+				Kieron doesn't have any <span Title="Linkedin doesn't count"><u>social media</u></span>. If you want to contact Kieron about business propositions, top secret missions, his stance on magicians, or a personal admission you can email him at <span className="text-link"> kieron@kieron.ca</span>.			</p>
 			</div>
 		</section>
 		</main>
