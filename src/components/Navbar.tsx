6a6ca0019edab374534cb9c9ee2601331e104eda
Kieron-vB <kierononvb@gmail.com>
2026-06-05 17:10:10 -0400

    Started on merch navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,9 +20,9 @@ export default function Navbar() {
         <a href="/" className="button py-0 pt-1">
     			<span>
       				<img src="/images/Logo.svg" alt="Logo" className="h-14 w-16 block" />
-    			</span>
-  		</a>        	
-		<a href="/contact" className="button"><span>Contact</span></a>
+    			</span>	</a>        	
+	<a href="/contact" className="button"><span>Contact</span></a>
+	<a href="/merch" className="button"><span>Merch</span></a>
         <a href="/pdfs/Engineering Resume - Kieron von Buchstab.pdf" target="_blank" rel="noopener noreferrer" className="button"><span>Resume</span></a>
       	</nav>
       	<div 
@@ -43,19 +43,19 @@ export default function Navbar() {
       <div className={`absolute top-0 left-0 w-3/4 h-screen bg-background z-20 p-8 shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
           <div className="flex justify-between">
             <a href="/" className="button">
-              <span>
-                <img src="/images/Logo.svg" alt="Logo" className="h-[6vh] w-[6vh]" />
-              </span>
-            </a>
+	    	<span>
+                	<img src="/images/Logo.svg" alt="Logo" className="h-[6vh] w-[6vh]" />
+              	</span></a>
             <button className="button" onClick={toggleMenu}>
-              <span>
-                <img src="/images/close.svg" alt="Close" className="h-[5vh] w-[5vh]" />
-              </span>
+	    	<span>
+			<img src="/images/close.svg" alt="Close" className="h-[5vh] w-[5vh]" />
+		</span>
             </button>
           </div>
           <a href="/about" className="button text-left text-2xl"><span>About</span></a>
           <a href="/pubs" className="button text-left text-2xl"><span>Publications</span></a>
           <a href="/contact" className="button text-left text-2xl"><span>Contact</span></a>
+	  <a href="/merch" className="button text-left text-2xl"><span>Merch</span></a>
           <a href="/pdfs/Engineering Resume - Kieron von Buchstab.pdf" target="_blank" rel="noopener noreferrer" className="button text-left text-2xl"><span>Resume</span></a>
         </div>
     </div>
